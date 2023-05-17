6a29705847aef9b35b54157b51f6f86ef3bba598
Pardofelis <90970780+AkazaRedSky@users.noreply.github.com>
2023-05-17 19:21:31 +0700

    feat: added CVA library
    Change:
    - Added Create Variance Authority Library
    - Added Folder for CVA Custom Style Component

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+#CHANGELOG
+
+## 17 - 05 - 2023
+
+- New : Added Class Variant Authority library
+- New : Added new custom Tailwind variants (text_variants and button_variants)
+- Fix : Deleted unnecessary Readme copy.md
---
+++ components/custom/custom.js
@@ -0,0 +1,68 @@
+
+import { cva, VariantProps } from 'class-variance-authority'
+
+//The naming convention and variants are temporary, feel free to edit or give suggestion
+const button_variants = cva(
+    '',
+    {
+      variants: {
+        variant: {
+          default:
+            '',
+            // destructive : deletion, cancel, etc.
+          destructive: 
+            '',
+            // acceptance : OK , Submit, etc.
+          acceptance:
+            '',
+            // info : like details, etc.
+          info:
+            '',
+            // clear enough  
+          disabled: '',
+            //To highlight a button
+          highlight: '',
+        },
+        size: {
+          default: 'rounded-md',
+          sm: 'rounded-md',
+          md: 'rounded-md',
+          lg: 'rounded-md',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+        size: 'default',
+      },
+    }
+  )
+  
+  const text_variants = cva(
+    '',
+    {
+      variants: {
+        variant: {
+          default:
+            '',
+          x: 
+            '',
+          y:
+            '',
+          z:
+            '',
+        },
+        size: {
+          default: '',
+          sm: '',
+          md: '',
+          lg: '',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+        size: 'default',
+      },
+    }
+  )
+
+  export { button_variants, text_variants }
