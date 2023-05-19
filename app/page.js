a2745abb9b6bb1d0d5e30493589134729c49194c
Melkijo <jonathanandaraa@gmail.com>
2023-05-19 15:40:06 +0800

    feat: add button

--- app/page.js
+++ app/page.js
@@ -1,5 +1,21 @@
+import { button_variants } from "@/components/custom/custom";
 export default function Home() {
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">Homepage</main>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         Homepage
+         <button className={button_variants()}>Button</button>
+         <button className={button_variants({ variant: 'danger' })}>Button<span>T</span></button>
+         <button className={button_variants({ variant: 'success' })}>Button</button>
+         <button className={button_variants({ variant: 'warning' })}>Button</button>
+         {/* <button className={button_variants({ variant: 'warning', intent: 'secondary' })}>Button</button> */}
+         <div className=" w-40">
+            <button className={button_variants({ variant: 'warning', size: 'full' })}>Button</button>
+         </div>
+         <button className={button_variants({ variant: 'outline_primary' })}>Button</button>
+         <button className={button_variants({ variant: 'outline_danger' })}>Button</button>
+         <button className={button_variants({ variant: 'disable' })}>Button</button>
+
+
+      </main>
    );
 }
--- components/custom/custom.js
+++ components/custom/custom.js
@@ -2,67 +2,67 @@
 import { cva, VariantProps } from 'class-variance-authority'
 
 //The naming convention and variants are temporary, feel free to edit or give suggestion
-const button_variants = cva(
-    '',
-    {
-      variants: {
-        variant: {
-          default:
-            '',
-            // destructive : deletion, cancel, etc.
-          destructive: 
-            '',
-            // acceptance : OK , Submit, etc.
-          acceptance:
-            '',
-            // info : like details, etc.
-          info:
-            '',
-            // clear enough  
-          disabled: '',
-            //To highlight a button
-          highlight: '',
-        },
-        size: {
-          default: 'rounded-md',
-          sm: 'rounded-md',
-          md: 'rounded-md',
-          lg: 'rounded-md',
-        },
-      },
-      defaultVariants: {
-        variant: 'default',
-        size: 'default',
-      },
-    }
-  )
-  
-  const text_variants = cva(
-    '',
-    {
-      variants: {
-        variant: {
-          default:
-            '',
-          x: 
-            '',
-          y:
-            '',
-          z:
-            '',
-        },
-        size: {
-          default: '',
-          sm: '',
-          md: '',
-          lg: '',
-        },
-      },
-      defaultVariants: {
-        variant: 'default',
-        size: 'default',
-      },
-    }
-  )
+const button_variants = cva(['font-normal', 'rounded-lg', 'gap-3', 'flex', 'justify-center']
+  ,
+  {
+    variants: {
+      variant: {
+        default:
+          ['bg-[#8EBF59]', 'hover:bg-[#63863E] hover:drop-shadow-md', 'text-white'],
+        danger:
+          ['bg-[#B1514D]', 'hover:bg-[#A12D28] hover:drop-shadow-md', 'text-white'],
+        success:
+          ['bg-[#517D67]', 'hover:bg-[#2D6248] hover:drop-shadow-md', 'text-white'],
+        warning:
+          ['bg-[#FEDE2D]', 'hover:bg-[#B29701] hover:drop-shadow-md', 'text-white'],
+        outline_primary:
+          ['bg-none', 'border-[#8EBF59]', 'text-[#8EBF59]', 'border', 'hover:bg-[#EBEBEB]'],
+        outline_danger:
+          ['bg-none', 'border-[#B1514D]', 'text-[#B1514D]', 'border', 'hover:bg-[#EBEBEB]'],
+        disable:
+          ['bg-[#DEDEDE]', 'text-[#858585] cursor-not-allowed'],
 
-  export { button_variants, text_variants }
+      },
+
+      size: {
+        default: ['py-3', 'px-4'],
+        full: ['w-full', 'py-3']
+
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+)
+
+const text_variants = cva(
+  '',
+  {
+    variants: {
+      variant: {
+        default:
+          '',
+        x:
+          '',
+        y:
+          '',
+        z:
+          '',
+      },
+      size: {
+        default: '',
+        sm: '',
+        md: '',
+        lg: '',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+)
+
+export { button_variants, text_variants }
