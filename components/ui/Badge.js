3e7fc3da98ea7ed1df7d04ff1b6b7c04da3f40ae
Pardofelis <90970780+AkazaRedSky@users.noreply.github.com>
2023-05-22 10:26:10 +0700

    feat : Fixed Avatar and Badge
    Changes:
    - Avatar and badge now fully functional

--- components/ui/Badge.js
+++ components/ui/Badge.js
@@ -1,9 +1,32 @@
-import React from 'react';
+'use client';
 
-export default function Badge(props) {
+import React,{ useState, useEffect } from 'react';
+
+// The colour is temporary, need confirmation
+export default function Badge({status}) {
+    const [status_colour, set_statuscolour] = useState('');
+
+    // there's probably better and more efficient way of doing this - need help...
+    /*  if the status is ONLINE  then the colour is GREEN
+        if the status is BUSY    then the colour is RED (?)
+        if the status is OFFLINE then the colour is GRAY
+        probably also add do not disturb but, need confirmation first!
+    */
+    useEffect(() => {
+      if (status === 'online') {
+        set_statuscolour('#1ab800') //from the figma 
+      } else if (status === 'busy') {
+        set_statuscolour('#b80000')
+      } else {
+        set_statuscolour('#a3a3a3')
+      }
+    
+    }, [status])
+    
+
   return (
-    <>
-      <span className={props.className}>Tahun</span>
-    </>
+    <div className="absolute top-0 right-0 translate-y-[5.8px] -translate-x-[6.28px]">
+      <div className={`bg-web-green-500 rounded-[50%] w-[15.7px] h-[14.52px] relative`}></div>
+    </div>
   );
 }
