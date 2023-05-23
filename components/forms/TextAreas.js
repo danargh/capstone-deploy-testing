b6ef038087ab640e8b13332830d6c349cd216a26
aryaganteng <hadi.argaya@gmail.com>
2023-05-23 14:39:48 +0700

    arya add tabel-textfield

--- components/forms/TextAreas.js
+++ components/forms/TextAreas.js
@@ -10,6 +10,23 @@ export const TextAreaArtikel = () =>{
     )
 }
 
+export const TextAreaContact = () =>{
+    return(
+        <div>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tuliskan Pesanmu Disini..."></textarea>
+        </div>
+    )
+}
+
+export const TextAreaPesan = () =>{
+    return(
+        <div>
+            <label>Pesan</label>
+            <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder=""></textarea>
+        </div>
+    )
+}
+
 export const TextAreaQuill = () => {
     const [value, setValue] = useState('');
     return(
