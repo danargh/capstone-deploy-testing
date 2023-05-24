74b074255e2234665817f3ef5247ee3ae21f09e8
Melkijo <jonathanandaraa@gmail.com>
2023-05-24 09:28:15 +0800

    feat: add home page

--- app/page.js
+++ app/page.js
@@ -1,62 +1,97 @@
 "use client"
-import { button_variants, text_variants_inter, input_variants } from "@/components/custom/custom";
-import Input from "@/components/forms/Input";
-import InputFile from "@/components/forms/input-file";
-import Card from "@/components/ui/Card";
-import detailArtikel from "../public/assets/images/detail-article.png"
-import Select from "@/components/forms/Select";
 import 'flowbite';
-import Radio from "@/components/forms/Radio";
-import InputSearch from "@/components/forms/input-search";
+
+import Card from "@/components/ui/Card";
+
+import heroImg from "../public/assets/images/hero.png"
+import hero2Img from "../public/assets/images/about-us-2-video.png"
+import hero3Img from "../public/assets/images/about-us-1.png"
+import detailArtikel from "../public/assets/images/detail-article.png"
+
+import Image from 'next/image';
+import Pagination from "@/components/ui/Pagination";
+import { useState } from "react";
+
 export default function Home() {
-    const handleSelectedFile = (selectedFile) => {
-        console.log(selectedFile);
-    };
+
+    const [currentPage, setCurentPage] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(12);
+
+    const lastPostIndex = currentPage * postPerPage;
+    const firstPostIndex = lastPostIndex - postPerPage;
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-5">
-            Homepage
-            <h2>Colors</h2>
-            <button className={button_variants()}>Button</button>
-            <button className={button_variants({ variant: "danger" })}>
-                Button<span>T</span>
-            </button>
-            <button className={button_variants({ variant: "success" })}>Button</button>
-            <button className={button_variants({ variant: "warning" })}>Button</button>
-            {/* <button className={button_variants({ variant: 'warning', intent: 'secondary' })}>Button</button> */}
-            <div className=" w-40">
-                <button className={button_variants({ variant: "warning", size: "full" })}>
-                    Button
-                </button>
+        <main className="flex min-h-screen flex-col items-center ">
+            <div id="default-carousel" className="relative w-full" data-carousel="slide">
+                {/* Carousel wrapper */}
+                <div className="relative h-[704px] overflow-hidden ">
+                    {/* Item 1 */}
+                    <div className="hidden duration-500  ease-in-out w-full   h-[704px]" data-carousel-item>
+                        <Image
+                            src={heroImg.src}
+                            fill={true}
+                            alt="hero-img"
+                        />
+                    </div>
+                    {/* Item 2 */}
+                    <div className="hidden duration-500 ease-in-out w-full h-[704px]" data-carousel-item>
+                        <Image
+                            src={hero2Img.src}
+                            fill={true}
+
+                            alt="hero-img"
+                        />
+                    </div>
+                    {/* Item 3 */}
+                    <div className="hidden duration-500  ease-in-out w-full h-[704px]" data-carousel-item>
+                        <Image
+                            src={hero3Img.src}
+                            fill={true}
+                            alt="hero-img"
+                        />
+                    </div>
+
+                </div>
+                {/* Slider indicators */}
+                <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                    <button
+                        type="button"
+                        className="w-4 h-4 rounded-full"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                        data-carousel-slide-to={0}
+                    />
+                    <button
+                        type="button"
+                        className="w-4 h-4 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 2"
+                        data-carousel-slide-to={1}
+                    />
+                    <button
+                        type="button"
+                        className="w-4 h-4 rounded-full"
+                        aria-current="false"
+                        aria-label="Slide 3"
+                        data-carousel-slide-to={2}
+                    />
+
+                </div>
+
             </div>
-            <button className={button_variants({ variant: "outline_primary" })}>Button</button>
-            <button className={button_variants({ variant: "outline_danger" })}>Button</button>
-            <button className={button_variants({ variant: "disable" })}>Button</button>
-            <h2>Typografi</h2>
-            <p className={text_variants_inter({ variant: "inter_24_bold" })}>Inter-bold-24</p>
-            <p className={text_variants_inter({ variant: "inter_32_bold" })}>Inter-bold-32</p>
-            <p className={text_variants_inter({ variant: "inter_22_bold" })}>Inter-bold-22</p>
-            <p className={text_variants_inter({ variant: "inter_20_bold" })}>Inter-bold-20</p>
-            <p className={text_variants_inter({ variant: "inter_32_semiBold" })}>Inter-semiBold-32</p>
-            <p className={text_variants_inter({ variant: "inter_24_semiBold" })}>Inter-semiBold-24</p>
-            <p className={text_variants_inter({ variant: "inter_32_reguler" })}>Inter-reguler-32</p>
-            <p className={text_variants_inter({ variant: "inter_24_reguler" })}>Inter-reguler-24</p>
-            <p className={text_variants_inter({ variant: "inter_22_reguler" })}>Inter-reguler-22</p>
-
-            <p>Input</p>
-            <Input className={input_variants()} type="text" placeholder="Masukan judul artikel" />
-            <InputSearch />
-            <Input className={input_variants({ variant: "dokter_login" })} type="text" placeholder="Username" />
-            <textarea className={input_variants({ variant: "textarea" })} placeholder="Comment" />
-            <Input className={input_variants({ variant: "chat_text" })} type="text" />
-            <textarea className={input_variants({ variant: "chat_textarea" })} placeholder="Comment" />
-            <Input className={input_variants({ variant: "detail_artikel" })} type="text" placeholder="Masukan judul artikel" />
+            <div className="grid grid-cols-3 gap-10 mt-[102px]">
+                {Array(15).fill(<Card images={detailArtikel} title="Ilmuwan di China Sebut COVID-19 Mungkin Berasal dari Manusia, Begini Temuannya
+          " description="Seorang ilmuwan di China mengungkapkan kemungkinan COVID-19 berasal dari manusia. Hal ini menyusul desakan dari Organisasi Kesehatan Dunia (WHO) kepada China untuk bersikap transparan perihal data asal-usul virus Corona." href="www.google.com" />).slice(firstPostIndex, lastPostIndex)}
 
 
-            <InputFile handleSelectedFile={handleSelectedFile} />
-            <Card images={detailArtikel} description="hai" title="ini card" href="www.google.com" />
-            <Select />
-            <Input className={input_variants({ variant: "contact_us" })} type="text" placeholder="Masukan judul artikel" />
-            <Radio />
+            </div>
+            <div className="flex justify-center mb-36">
+                <Pagination
+                    totalPosts={15}
+                    postPerPage={postPerPage}
+                    setCurrentPage={setCurentPage}
+                    currentPage={currentPage}
+                />
+            </div>
         </main>
     )
 }
--- components/ui/Card.js
+++ components/ui/Card.js
@@ -6,16 +6,16 @@ import Link from "next/link";
 export default function Card({ images, title, description, href }) {
     return (
         <>
-            <div className="flex flex-col items-start isolation-auto absolute w-[410px] h-[528px] left-[74px] top-[202px] bg-neutral-0 shadow-md rounded-lg border border-gray-300">
+            <div className="flex flex-col items-start isolation-auto  w-[410px] h-[528px]  bg-neutral-0 shadow-md rounded-lg border border-neutral-40">
                 <div className="w-[410px] h-[256px] rounded-t-lg  overflow-hidden">
                     <Image src={images} alt="Title" width={410} height={256} />
                 </div>
                 <div className="flex flex-col items-center w-[410px] h-[248px]">
                     <div className="flex w-96 h-fit items-center justify-center px-4 -mt-24 bg-neutral-0 rounded-t-xl">
-                        <p className="font-inter text-center text- font-bold text-neutral-900">{title}</p>
+                        <p className="pt-3 font-inter text-center text- font-bold text-neutral-900">{title}</p>
                     </div>
                     <div className="flex w-96 h-fit items-start justify-start px-4 py-8 bg-neutral-0 rounded-none">
-                        <p className="font-inter text-justify text-[14px] font-normal text-[#6C6C6C]">
+                        <p className="font-inter text-justify text-[14px] font-normal text-neutral-300">
                             {description}
                         </p>
                     </div>
