b25cfa21478950cc73dd5d95e45f56a95c3adcca
Melkijo <jonathanandaraa@gmail.com>
2023-05-28 13:08:41 +0800

    fix: fillterButton

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -4,33 +4,33 @@ import { FilterIcon } from "@/public/assets/icons/icons";
 import { button_variants, text_variants_inter, text_variants_poppins } from "../custom/custom";
 
 export const DetailDokterButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
+        </button>
+    );
 };
 
 export const DaftarWhatsAppButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "outline_primary", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "lg" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "outline_primary", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "lg" })}>{children}</p>
+        </button>
+    );
 };
 export const DaftarEmailButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "lg" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "lg" })}>{children}</p>
+        </button>
+    );
 };
 export const KirimKomentarButton = ({ onClick, children }) => {
-   return (
-      <button className={` w-[150px] flex items-center py-[16px] ${button_variants({ variant: "default", size: "default" })}`} onClick={onClick}>
-         <p className="font-poppins font-[600] text-[14px] leading-4">{children}</p>
-      </button>
-   );
+    return (
+        <button className={` w-[150px] flex items-center py-[16px] ${button_variants({ variant: "default", size: "default" })}`} onClick={onClick}>
+            <p className="font-poppins font-[600] text-[14px] leading-4">{children}</p>
+        </button>
+    );
 };
 
 export const LoginDokterButton = ({ onClick, children }) => {
@@ -42,81 +42,82 @@ export const LoginDokterButton = ({ onClick, children }) => {
 };
 
 export const TransferSaldo = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
+        </button>
+    );
 };
 export const FilterButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "ic_black", size: "default" })} onClick={onClick}>
-         <div style={{ display: "flex", alignItems: "center" }}>
-            <p className={text_variants_inter({ variant: "inter_24_reguler" })}>{children}</p>
-         </div>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "ic_black", size: "default" })} onClick={onClick}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <FilterButton fill="white" />
+                <p className={text_variants_inter({ variant: "inter_24_reguler" })} style={{ color: "white" }}>{children}</p>
+            </div>
+        </button>
+    );
 };
 
 export const KirimPesanButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_20_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_20_reguler" })}>{children}</p>
+        </button>
+    );
 };
 export const AddObatButton = ({ onClick }) => {
-   return (
-      <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
+        </button>
+    );
 };
 
 export const SendObatButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_24_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_24_reguler" })}>{children}</p>
+        </button>
+    );
 };
 export const KirimArtikelButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
+        </button>
+    );
 };
 
 export const SimpanArtikelButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "xs" })}>{children}</p>
+        </button>
+    );
 };
 
 export const LanjutkanProfileButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
+        </button>
+    );
 };
 export const SimpanProfileButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
+        </button>
+    );
 };
 
 export const BatalHapusArtikelButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "outline_danger", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "xl" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "outline_danger", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "xl" })}>{children}</p>
+        </button>
+    );
 };
 
 export const HapusArtikelButton = ({ onClick, children }) => {
@@ -128,11 +129,11 @@ export const HapusArtikelButton = ({ onClick, children }) => {
 };
 
 export const ContinueWithdrawButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_24_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_24_reguler" })}>{children}</p>
+        </button>
+    );
 };
 
 export const LanjutkanWithdrawButton = ({ onClick, children }) => {
@@ -143,29 +144,29 @@ export const LanjutkanWithdrawButton = ({ onClick, children }) => {
     );
 };
 export const TutupWithdrawButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
-         <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "default", size: "default" })} onClick={onClick}>
+            <p className={text_variants_inter({ variant: "inter_32_reguler" })}>{children}</p>
+        </button>
+    );
 };
 
 export const SignInButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
-         <p className={text_variants_poppins({ size: "sm" })}>{children}</p>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
+            <p className={text_variants_poppins({ size: "sm" })}>{children}</p>
+        </button>
+    );
 };
 
 export const RegisterButton = ({ onClick, children }) => {
-   return (
-      <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
-         <div style={{ display: "flex", alignItems: "center" }}>
-            <p className={text_variants_poppins({ size: "sm" })}>{children}</p>
-         </div>
-      </button>
-   );
+    return (
+        <button className={button_variants({ variant: "green_bg", size: "default" })} onClick={onClick}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <p className={text_variants_poppins({ size: "sm" })}>{children}</p>
+            </div>
+        </button>
+    );
 
 };
 
