50cbac52785755fea3b7f1152e550e3a12ebc552
rinehaxor <rinehaxor@users.noreply.github.com>
2023-05-30 20:05:45 +0700

    fix: desain tabel artikel

--- app/accordiontest/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Accordion from '../../components/ui/Accordion';
-export default function Page() {
-   return (
-      <>
-         <Accordion />
-      </>
-   );
-}
