3bc25b8b9c2ebd268cabfcbcba89735cfe13fd1a
Riel <90970780+AkazaRedSky@users.noreply.github.com>
2023-06-03 10:51:38 +0700

    fix: Fixed Alternate Pagination
    - Fixed pagination not setting the pages correctly
    - Fixed pagination displaying the wrong colour

--- components/ui/PaginationAlt.js
+++ components/ui/PaginationAlt.js
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
 
-export default function PaginationAlt( {currentPage, articlePerPages, articleData} ) {
+/* Example of Page Change Function :
+   const handlePageChange = (page) => {
+      setCurrentPage(page); // Set the current Page
+      setBaseIndex(1 + articlesPerPage * (page-1)); // To set the base indexes
+   };
+*/
+
+// Might later make a dropdown for the pages if necessary
+export default function PaginationAlt( {currentPage, totalPages, onPageChange} ) {
    const [cPage, setCurrentPage] = useState(currentPage);
-
-   // Calculate the indexes of the articles to be displayed on the current page
-   const indexOfLastArticle = cPage * articlePerPages;
-   const indexOfFirstArticle = indexOfLastArticle - articlePerPages;
-
-   // Slice the articleData array to get the articles for the current page
-   const currentArticles = articleData
-      ? articleData.slice(indexOfFirstArticle, indexOfLastArticle)
-      : [];
-
-   // Calculate the total number of pages based on the articlePerPages
-   const totalPages = Math.ceil((articleData?.length || 0) / articlePerPages);
 
    // Function to handle the next page
    const nextPage = () => {
       if (cPage < totalPages) {
          setCurrentPage(cPage + 1);
+         onPageChange(cPage + 1);
       }
    };
 
@@ -26,31 +23,30 @@ export default function PaginationAlt( {currentPage, articlePerPages, articleDat
    const prevPage = () => {
       if (cPage > 1) {
          setCurrentPage(cPage - 1);
+         onPageChange(cPage - 1);
       }
    };
 
    return (
       <div className="flex flex-row gap-0 items-start justify-start shrink-0 w-[238px] relative">
          <div
-            className="bg-[#d1d8c8] rounded-[5px_0px_0px_5px] pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center flex-1 h-[45px] relative"
+            className={`select-none font-poppins font-normal text-xs/[120%] text-neutral-0 text-left rounded-[5px_0px_0px_5px] pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center flex-1 h-[45px] relative ${
+                cPage === 1 ? "cursor-not-allowed bg-[#d1d8c8]" : "cursor-pointer bg-web-green-300"}`}
             onClick={prevPage}
          >
-            <div className="font-poppins font-normal text-xs/[120%] text-neutral-0 text-left relative">
                Sebelum
-            </div>
          </div>
-         <div className="border-solid border-web-green-300 border pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center shrink-0 w-[60px] h-[45px] relative">
+         <div className="select-none border-solid border-web-green-300 border pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center shrink-0 w-[60px] h-[45px] relative">
             <div className="font-poppins font-normal text-xs/[120%] text-[#7CA153] text-left relative">
                {cPage}
             </div>
          </div>
          <div
-            className="bg-web-green-300 rounded-[0px_5px_5px_0px] pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center flex-1 h-[45px] relative"
+            className={`select-none font-poppins font-normal text-xs/[120%] text-neutral-0 text-left  rounded-[0px_5px_5px_0px] pt-4 pr-3 pb-4 pl-3 flex flex-row gap-2.5 items-center justify-center flex-1 h-[45px] relative ${
+                cPage === totalPages ? "cursor-not-allowed bg-[#d1d8c8]" : "cursor-pointer bg-web-green-300"}`}
             onClick={nextPage}
          >
-            <div className="font-poppins font-normal text-xs/[120%] text-neutral-0 text-left relative">
                Lanjut
-            </div>
          </div>
       </div>
    );
