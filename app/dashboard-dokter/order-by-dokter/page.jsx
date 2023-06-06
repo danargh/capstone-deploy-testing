10c9d317f377b27aba8b24abbb364854af020a1f
Melkijo <jonathanandaraa@gmail.com>
2023-06-06 15:46:20 +0800

    melki-fix: navbar

--- app/dashboard-dokter/order-by-dokter/page.jsx
+++ app/dashboard-dokter/order-by-dokter/page.jsx
@@ -1,70 +1,116 @@
 "use client";
 
 import { FilterButton } from "@/components/ui/Button";
-import Footer from "@/components/ui/Footer";
-import NavbarDokter from "@/components/ui/NavbarDokter";
+import { PaginationOrderDokter } from "@/components/ui/Pagination";
 import { TableOrder } from "@/components/ui/Table";
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 function page() {
    const dataDokter = [
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Berlanjut",
          chat: true,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Selesai",
          chat: false,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Selesai",
          chat: true,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Berlanjut",
          chat: false,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Berlanjut",
          chat: false,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Berlanjut",
          chat: false,
       },
       {
          date: "12/05/2023",
          name: "Farhan",
          gender: "L",
-         status: "Online",
+         status: "Berlanjut",
          chat: false,
       },
    ];
+   const orderLength = dataDokter.length;
+   const [orderShow, setOrderShow] = useState("1");
+   const [currentPage, setCurentPage] = useState(1);
+   const [postPerPage, setPostPerPage] = useState(orderShow);
+
+   useEffect(() => {
+      setPostPerPage(orderShow);
+      setCurentPage(1);
+   }, [orderShow]);
+   const lastPostIndex = currentPage * postPerPage;
+   const firstPostIndex = lastPostIndex - postPerPage;
+   const handleSelectChange = (event) => {
+      setOrderShow(event.target.value);
+   };
    return (
       <>
          <div className=" max-w-[1440px] mx-auto px-[32px]">
-            <h1 className="text-inter text-xl font-bold mt-[50px] mb-[20px]">Pesanan</h1>
+            <h1 className="text-inter text-xl font-bold mt-[50px] mb-[20px]">
+               Pesanan
+            </h1>
             <FilterButton>Filter Data</FilterButton>
-
-            <TableOrder order={dataDokter} />
+            <div className="relative flex py-10">
+               <p className="py-2">Menampilkan</p>
+               <select
+                  id="data-per-page"
+                  className="appearance-none bg-white border border-gray-300 rounded-xl px-4 py-2 pr-8 leading-tight focus:outline-none focus:border-blue-500 mx-5"
+                  value={orderShow}
+                  onChange={handleSelectChange}
+               >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="5">5</option>
+                  <option value="10">10</option>
+               </select>
+               <p className="py-2">Data</p>
+            </div>
+            <TableOrder
+               order={dataDokter}
+               firstPostIndex={firstPostIndex}
+               lastPostIndex={lastPostIndex}
+            />
+            <div className="flex justify-between items-center mt-10 mb-24">
+               <div>
+                  Menampilkan {firstPostIndex + 1} ke{" "}
+                  {lastPostIndex > orderLength ? orderLength : lastPostIndex}{" "}
+                  dari {orderLength} data
+               </div>
+               <PaginationOrderDokter
+                  totalPosts={orderLength}
+                  postPerPage={postPerPage}
+                  setCurrentPage={setCurentPage}
+                  currentPage={currentPage}
+                  orderShow={orderShow}
+               />
+            </div>
          </div>
       </>
    );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 
-import "flowbite";
 import Card from "@/components/ui/Card";
 import detailArtikel from "../public/assets/images/detail-article.png";
 import Navbar from "@/components/ui/Navbar";
@@ -8,35 +7,54 @@ import Footer from "@/components/ui/Footer";
 import { Pagination } from "@/components/ui/Pagination";
 import { useState } from "react";
 import Carousel from "@/components/ui/Carousel";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-   const [currentPage, setCurentPage] = useState(1);
-   const [postPerPage, setPostPerPage] = useState(12);
+    const [currentPage, setCurentPage] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(12);
 
-   const lastPostIndex = currentPage * postPerPage;
-   const firstPostIndex = lastPostIndex - postPerPage;
-   return (
-      <>
-         <Navbar />
-         <main className="flex min-h-screen flex-col items-center">
-            <Carousel />
-            <div className="grid grid-cols-3 gap-10 mt-[102px]" id="article">
-               {Array(15)
-                  .fill(
-                     <Card
-                        images={detailArtikel}
-                        title="Ilmuwan di China Sebut COVID-19 Mungkin Berasal dari Manusia, Begini Temuannya"
-                        description="Seorang ilmuwan di China mengungkapkan kemungkinan COVID-19 berasal dari manusia. Hal ini menyusul desakan dari Organisasi Kesehatan Dunia (WHO) kepada China untuk bersikap transparan perihal data asal-usul virus Corona."
-                        href="www.google.com"
-                     />
-                  )
-                  .slice(firstPostIndex, lastPostIndex)}
-            </div>
-            <div className="flex justify-center mb-36">
-               <Pagination totalPosts={15} postPerPage={postPerPage} setCurrentPage={setCurentPage} currentPage={currentPage} />
-            </div>
-         </main>
-         <Footer />
-      </>
-   );
+    const lastPostIndex = currentPage * postPerPage;
+    const firstPostIndex = lastPostIndex - postPerPage;
+
+    const router = useRouter();
+    const handleDetailArticle = (id) => {
+        router.push(`/article/${id}`);
+    };
+    const newData = [];
+    const totalPosts = 26;
+    for (let i = 0; i < totalPosts; i++) {
+        const item = {
+            images: detailArtikel,
+            title: "Ilmuwan di China Sebut COVID-19 Mungkin Berasal dari Manusia, Begini Temuannya",
+            description: "Seorang ilmuwan di China mengungkapkan kemungkinan COVID-19 berasal dari manusia. Hal ini menyusul desakan dari Organisasi Kesehatan Dunia (WHO) kepada China untuk bersikap transparan perihal data asal-usul virus Corona.",
+        };
+        const newItem = {
+            ...item,
+        };
+        newData.push(newItem);
+    }
+    return (
+        <>
+            <Navbar />
+            <main className="flex min-h-screen flex-col items-center">
+                <Carousel />
+                <div className="grid grid-cols-3 gap-10 mt-[102px]" id="article">
+                    {newData.slice(firstPostIndex, lastPostIndex).map((item, index) => (
+                        <Card
+                            key={index}
+                            images={item.images}
+                            title={item.title}
+                            description={item.description}
+                            postId={1}
+                            handleDetailArticle={handleDetailArticle}
+                        />
+                    ))}
+                </div>
+                <div className="flex justify-center mb-36">
+                    <Pagination totalPosts={totalPosts} postPerPage={postPerPage} setCurrentPage={setCurentPage} currentPage={currentPage} />
+                </div>
+            </main>
+            <Footer />
+        </>
+    );
 }
