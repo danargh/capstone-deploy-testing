ef0867a9fec6da7f75b9f5eaf266c06c3757eb23
Melkijo <jonathanandaraa@gmail.com>
2023-06-08 21:02:52 +0800

    melki-fix: help responsive

--- app/help/akses-prevent/page.jsx
+++ app/help/akses-prevent/page.jsx
@@ -1,16 +1,14 @@
-"use client";
-import React from "react";
 import { aksesPrevent } from "../dataHelp";
 import { AccordionHelp } from "@/components/ui/Accordion";
 
 function page() {
    return (
       <>
-         <div className=" flex items-center flex-col ">
+         <div className=" flex items-center flex-col px-5 ">
             <div className=" font-semibold text-[34px] mb-[47px]">
                Mengakses Prevent
             </div>
-            <div className="w-[1088px]">
+            <div className="w-full sm:w-[1088px]">
                {aksesPrevent.map((item) => (
                   <AccordionHelp
                      question={item.question}
--- app/help/harga-pembayaran/page.jsx
+++ app/help/harga-pembayaran/page.jsx
@@ -1,16 +1,14 @@
-"use client";
-import React from "react";
 import { hargaPembayaran } from "../dataHelp";
 import { AccordionHelp } from "@/components/ui/Accordion";
 
 function page() {
    return (
       <>
-         <div className=" flex items-center flex-col ">
+         <div className=" flex items-center flex-col">
             <div className=" font-semibold text-[34px] mb-[47px]">
                Harga dan Pembayaran
             </div>
-            <div className="w-[1088px]">
+            <div className="w-full sm:w-[1088px]  px-5">
                {hargaPembayaran.map((item) => (
                   <AccordionHelp
                      question={item.question}
