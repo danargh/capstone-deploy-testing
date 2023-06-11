f8d06017f97ed7a24a30391f649727a02ecd5bf6
Melkijo <jonathanandaraa@gmail.com>
2023-06-11 23:04:12 +0800

    melki-fix: update

--- components/ui/Navbar.js
+++ components/ui/Navbar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Image from "next/image";
 import { SearchIcon } from "@/public/assets/icons/icons";
 import Link from "next/link";
