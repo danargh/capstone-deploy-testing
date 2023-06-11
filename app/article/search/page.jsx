7c16d1cad79fa9a4db7bf0e4275f5fcd735bec26
Melkijo <jonathanandaraa@gmail.com>
2023-06-11 22:40:36 +0800

    melki-fix

---
+++ app/article/search/page.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { useState } from "react";
+import Card from "@/components/ui/Card";
+import detailArtikel from "@/public/assets/images/detail-article.png";
+import { Pagination } from "@/components/ui/Pagination";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/ui/Navbar";
+import Footer from "@/components/ui/Footer";
+import { useSearchParams } from "next/navigation";
+export default function Article() {
+   const [currentPage, setCurentPage] = useState(1);
+   const [postPerPage, setPostPerPage] = useState(12);
+   const router = useRouter();
+   const lastPostIndex = currentPage * postPerPage;
+   const firstPostIndex = lastPostIndex - postPerPage;
+
+   const handleDetailArticle = (id) => {
+      router.push(`/article/${id}`);
+   };
+
+   const searchParams = useSearchParams();
+   const search = searchParams.get("q");
+   return (
+      <>
+         <Navbar />
+         <section className="font-inter font-[600] text-[20px] mt-[32px] mb-16 w-[1440px] mx-auto">
+            <p className="mb-[20px] leading-8">Hasil Pencarian "{search}"</p>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10">
+               {Array(15)
+                  .fill(
+                     <Card
+                        images={detailArtikel}
+                        title="Ilmuwan di China Sebut COVID-19 Mungkin Berasal dari Manusia, Begini Temuannya"
+                        description="Seorang ilmuwan di China mengungkapkan kemungkinan COVID-19 berasal dari manusia. Hal ini menyusul desakan dari Organisasi Kesehatan Dunia (WHO) kepada China untuk bersikap transparan perihal data asal-usul virus Corona."
+                        href="www.google.com"
+                        postId={1}
+                        handleDetailArticle={handleDetailArticle}
+                     />
+                  )
+                  .slice(firstPostIndex, lastPostIndex)}
+            </div>
+            <div className="flex justify-center mt-3">
+               <Pagination
+                  totalPosts={15}
+                  postPerPage={postPerPage}
+                  setCurrentPage={setCurentPage}
+                  currentPage={currentPage}
+               />
+            </div>
+         </section>
+         <Footer />
+      </>
+   );
+}
