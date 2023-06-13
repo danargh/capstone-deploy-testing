4473b2135c0ef3d39b68b1285b8e212a231b1d25
Riel <90970780+AkazaRedSky@users.noreply.github.com>
2023-06-13 20:21:09 +0700

    feat: whitelisted firebase storage

--- api/article.js
+++ api/article.js
@@ -1,15 +1,12 @@
-// Ftx Later - also the fucking api still can't be used
-// I REALLY gonna have a bad time integrating this shit
-// FFS I HAD LESS THAN A WEEK TO DO THIS SHIT
-// FUCK YOU
+import useSWR, { mutate } from "swr";
 
+// Fix Later
+
+const authToken = "lorem ipsum";
 export const fetcher = (url) =>
    fetch(url, {
       headers: {
-        // THE DAMN AUTH IS EXPIRED.... WHAT THE HELL MAN?
-         Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJleHAiOjE2ODU5ODUzMTUsImlkIjoyLCJuYW1lIjoiZG9jdG9yIDEifQ._kTRqYbZEZ6MiFfZ5fDHpO8twM5wuLkOxHvadxbHHXw",
+         Authorization: 
+            `Bearer ${authToken}`,
       },
    }).then((res) => res.json());
-
-
