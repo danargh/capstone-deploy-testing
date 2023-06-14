5002723b612104f3f0018fc79d877f3ba8ab0eb5
Melkijo <jonathanandaraa@gmail.com>
2023-06-14 20:58:43 +0800

    melki-add: mockup homepage

--- app/page.js
+++ app/page.js
@@ -8,8 +8,11 @@ import { Pagination } from "@/components/ui/Pagination";
 import { useState } from "react";
 import Carousel from "@/components/ui/Carousel";
 import { useRouter } from "next/navigation";
+import { useArticles } from "@/components/atoms/useArticles";
+
 
 export default function Home() {
+    const articles = useArticles();
     const [currentPage, setCurentPage] = useState(1);
     const [postPerPage, setPostPerPage] = useState(12);
 
@@ -20,38 +23,26 @@ export default function Home() {
     const handleDetailArticle = (id) => {
         router.push(`/article/${id}`);
     };
-    const newData = [];
-    const totalPosts = 26;
-    for (let i = 0; i < totalPosts; i++) {
-        const item = {
-            images: detailArtikel,
-            title: "Ilmuwan di China Sebut COVID-19 Mungkin Berasal dari Manusia, Begini Temuannya",
-            description: "Seorang ilmuwan di China mengungkapkan kemungkinan COVID-19 berasal dari manusia. Hal ini menyusul desakan dari Organisasi Kesehatan Dunia (WHO) kepada China untuk bersikap transparan perihal data asal-usul virus Corona.",
-        };
-        const newItem = {
-            ...item,
-        };
-        newData.push(newItem);
-    }
+
     return (
         <>
             <Navbar />
             <main className="flex min-h-screen flex-col items-center">
                 <Carousel />
                 <div className="grid grid-cols-3 gap-10 mt-[102px]" id="article">
-                    {newData.slice(firstPostIndex, lastPostIndex).map((item, index) => (
+                    {articles && articles.slice(firstPostIndex, lastPostIndex).map((article, index) => (
                         <Card
                             key={index}
-                            images={item.images}
-                            title={item.title}
-                            description={item.description}
+                            images={detailArtikel}
+                            title={article.title}
+                            description={article.description}
                             postId={1}
                             handleDetailArticle={handleDetailArticle}
                         />
                     ))}
                 </div>
                 <div className="flex justify-center mb-36">
-                    <Pagination totalPosts={totalPosts} postPerPage={postPerPage} setCurrentPage={setCurentPage} currentPage={currentPage} />
+                    <Pagination totalPosts={articles ? articles.length : 0} postPerPage={postPerPage} setCurrentPage={setCurentPage} currentPage={currentPage} />
                 </div>
             </main>
             <Footer />
---
+++ components/atoms/useArticles.js
@@ -0,0 +1,20 @@
+import { atom, useAtom } from "jotai";
+import useSWR, { mutate } from 'swr'
+
+
+export const articlesAtom = atom(null);
+
+const fetchArticles = async () => {
+    const response = await fetch('https://64872a94beba6297279025c6.mockapi.io/articles');
+    return response.json();
+}
+
+export function useArticles() {
+    const [articles, setArticles] = useAtom(articlesAtom);
+
+    const { data: getArticles } = useSWR('articles', fetchArticles);
+    setArticles(getArticles);
+
+    return articles;
+}
+
