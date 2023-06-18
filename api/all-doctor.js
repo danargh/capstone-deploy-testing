05b6a7bb6fd87ef37d3d83651d4dbfcd352b5def
Riel <90970780+AkazaRedSky@users.noreply.github.com>
2023-06-19 00:01:02 +0700

    feat: Added API Integrations

---
+++ api/all-doctor.js
@@ -0,0 +1,28 @@
+import React from "react";
+import useSWR, { mutate } from "swr";
+import AWSLink from "./awslink";
+
+const fetcher = async (url, token) =>
+   fetch(url, {
+      headers: {},
+   }).then((res) => res.json());
+
+export default function FetchAllDoctor() {
+   // Endpoint
+   const { awslink } = AWSLink();
+   const doctorEndpoint = `${awslink}/doctors`;
+
+   //Fetch the datas
+   const {
+      data: fetchedDoctorData,
+      error: doctorError,
+      mutate: doctorMutate,
+   } = useSWR(doctorEndpoint, fetcher, {
+      revalidateOnFocus: false,
+   });
+
+   const doctorData = fetchedDoctorData ? fetchedDoctorData.doctors : [];
+   console.log(fetchedDoctorData);
+   console.log(doctorError);
+   return { doctorData, doctorError };
+}
---
+++ api/awslink.js
@@ -0,0 +1,15 @@
+import { atom, useAtom } from "jotai";
+
+const awsLinkAtom = atom(
+   "http://ec2-3-27-124-243.ap-southeast-2.compute.amazonaws.com:8080"
+);
+
+const authToken = atom(
+   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6MiwiYXV0aG9yaXplZCI6dHJ1ZSwiZXhwIjoxNjg3MzEyNzk0fQ.TvUgvWPn7mexTGGddqZ88RP-oIRhDmjsdkp3n2YjYIg"
+);
+
+export default function AWSLink() {
+   const [awslink] = useAtom(awsLinkAtom);
+   const [token] = useAtom(authToken);
+   return { awslink, token };
+}
