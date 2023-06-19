a8e32614bec10ea7f54e876125889bfb58d0fbef
Riel <90970780+AkazaRedSky@users.noreply.github.com>
2023-06-19 20:36:35 +0700

    feat: fixed crash when token does not exist

--- api/awslink.js
+++ api/awslink.js
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { atom, useAtom } from "jotai";
 import Cookies from "js-cookie";
@@ -7,17 +7,15 @@ import Cookies from "js-cookie";
 //    "https://ec2-3-27-124-243.ap-southeast-2.compute.amazonaws.com:8080"
 // );
 
-const awsLinkAtom = atom(
-       "https://capstone-project.duckdns.org:8080"
-    );
+const awsLinkAtom = atom("https://capstone-project.duckdns.org:8080");
 
 // const authToken = atom(
 //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbl9pZCI6MSwiYXV0aG9yaXplZCI6dHJ1ZSwiZXhwIjoxNjg3NDEwMTAzfQ.0ZCsPav8XwTRfsjuSiwaNGe5YkKy-BlgIi5wM_zfAOM"
 // );
 
 export default function AWSLink() {
-    const [token] = Cookies.get('adminToken');
+   const token = Cookies.get("adminToken") || "";
    const [awslink] = useAtom(awsLinkAtom);
-//    const [token] = useAtom(authToken);
+   //    const [token] = useAtom(authToken);
    return { awslink, token };
 }
--- app/dokter/loading.jsx
+++ app/dokter/loading.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function Loading() {
-  return (
-    <div>Loading...</div>
-  )
+   return (
+      <div className="flex justify-center items-center h-screen">
+         <div
+            className="inline-block h-16 w-16 animate-spin rounded-full border-[16px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_2.5s_linear_infinite]"
+         >
+            <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+               Loading...
+            </span>
+         </div>
+      </div>
+   );
 }
