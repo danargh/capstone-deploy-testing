5576479cc9b101764aef6de3324daf449afa6bf6
danargh <danargh86@gmail.com>
2023-06-19 11:14:55 +0700

    danar :  fixing login & obat integration

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 import "react-quill/dist/quill.snow.css";
+import {ErrorBoundary} from "react-error-boundary";
+
 
 export const metadata = {
    title: "Prevent",
--- components/atoms/useObatDoctor.js
+++ components/atoms/useObatDoctor.js
@@ -1,12 +1,56 @@
 import { atom, useAtom } from "jotai";
+import useSWR, { mutate } from "swr";
+import useSWRMutation from "swr/mutation";
+import Cookies from "js-cookie";
 
 export const namaObatAtom = atom([]);
+export const dataDrugsAtom = atom(["Fluoxetine", "Alprazolam", "Sertraline", "Lorazepam", "Antidepresan"]);
+
+const fetcher = (url) =>
+   fetch(url, {
+      headers: {
+         "Content-Type": "application/json",
+         Authorization: "Bearer " + Cookies.get("doctorToken"),
+      },
+      method: "GET",
+   }).then((res) => res.json());
+
+export function getDrugs() {
+   const [dataDrugs, setDataDrugs] = useAtom(dataDrugsAtom);
+
+   const { data, error } = useSWR("http://ec2-3-27-124-243.ap-southeast-2.compute.amazonaws.com:8080/doctor/drugs", fetcher, {
+      onSuccess: (data) => {
+         setDataDrugs(data);
+      },
+      onError: (error) => {
+         console.log(error);
+      },
+   });
+
+   return {
+      dataDrugs,
+      error,
+      isLoading: !error && !data,
+   };
+}
+
+const sendObatRequest = (url, { arg }) =>
+   fetch(url, {
+      headers: {
+         "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(arg),
+   }).then((res) => res.json());
 
 export function useObatDoctor() {
    const [namaObat, setNamaObat] = useAtom(namaObatAtom);
+   const { trigger, isMutating, data, error } = useSWRMutation("http://ec2-3-27-124-243.ap-southeast-2.compute.amazonaws.com:8080/doctor/recipt", sendObatRequest);
 
    return {
-      namaObat,
-      setNamaObat,
+      data,
+      error,
+      isObatLoading: isMutating,
+      triggerObat: ({ id, nama, jumlah }) => trigger({ id: id, nama: nama, jumlah: jumlah }),
    };
 }
