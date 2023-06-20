c0acfdeec6533b18e0dcb54c1c95047dc79dd7fb
Melkijo <jonathanandaraa@gmail.com>
2023-06-20 21:20:40 +0800

    fix: deploy part 2

--- app/help/page.jsx
+++ app/help/page.jsx
@@ -15,15 +15,15 @@ function page() {
                <div className="font-semibold text-lg">Harga dan pembayaran</div>
                <svg
                   data-accordion-icon
-                  class={"w-6 h-6 -rotate-90 shrink-0"}
+                  className={"w-6 h-6 -rotate-90 shrink-0"}
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                >
                   <path
-                     fill-rule="evenodd"
+                     fillRule="evenodd"
                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                     clip-rule="evenodd"
+                     clipRule="evenodd"
                   ></path>
                </svg>
             </Link>
@@ -34,15 +34,15 @@ function page() {
                <div className="font-semibold text-lg">Mengakses Prevent</div>
                <svg
                   data-accordion-icon
-                  class={"w-6 h-6 -rotate-90 shrink-0"}
+                  className={"w-6 h-6 -rotate-90 shrink-0"}
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                >
                   <path
-                     fill-rule="evenodd"
+                     fillRule="evenodd"
                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                     clip-rule="evenodd"
+                     clipRule="evenodd"
                   ></path>
                </svg>
             </Link>
@@ -53,8 +53,9 @@ function page() {
                Pertanyaan Berdasarkan Topik Anda
             </div>
             <div className="w-full sm:w-[1088px]">
-               {helpUtama.map((item) => (
+               {helpUtama.map((item, index) => (
                   <AccordionHelp
+                     key={index}
                      question={item.question}
                      answer={item.answer}
                   />
